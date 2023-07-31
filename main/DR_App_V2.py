ba322b6537e8fea9da3204753caa00b9ccffb146
Khanrasheed472 <khanrasheed472@hotmail.com>
2023-07-31 23:40:02 +0800

    update on adversarial attack
    can finally read images, but version issue on my end with transformers, but by right without this problem it must work.

--- main/DR_App_V2.py
+++ main/DR_App_V2.py
@@ -1,55 +1,45 @@
-import streamlit as st
-from PIL import Image
-import torch
-import torchvision.transforms as transforms
-import numpy as np
 import tensorflow as tf
-from transformers import ViTFeatureExtractor, ViTForImageClassification
+from PIL import Image
+from transformers import ViTFeatureExtractor, ViTImageProcessor
+import streamlit as st 
 import os
+import io
+import torch
+import numpy as np
 
+def create_adversarial_pattern(image, label, model, epsilon):
+    """Generates an adversarial pattern for the input image.
 
-# Define the loss function
-loss_object = tf.keras.losses.CategoricalCrossentropy()
+    Args:
+        image: The input image tensor.
+        label: The label of the image.
+        model: The pretrained ViT model.
+        epsilon: The epsilon value to use for the adversarial attack.
 
-# Function to create the adversarial pattern using FGSM
-def create_adversarial_pattern(input_image, input_label, model):
-    with tf.GradientTape() as tape:
-        tape.watch(input_image)
-        prediction = model(input_image)
-        loss = loss_object(input_label, prediction)
+    Returns:
+        The adversarial pattern tensor.
+    """
 
-    # Get the gradients of the loss w.r.t to the input image.
-    gradient = tape.gradient(loss, input_image)
-    # Get the sign of the gradients to create the perturbation
-    signed_grad = tf.sign(gradient)
-    return signed_grad
+    # Create a copy of the image tensor.
+    processed_image = image.clone().detach()
 
-# Function to generate adversarial attack using FGSM
-def generate_adversarial_attack(image, dr_class_index, model):
-    num_classes = model.config.num_labels
+    # Calculate the loss for the image.
+    loss = model(processed_image).logits[label]
 
-    label = tf.one_hot(dr_class_index, num_classes)
-    label = tf.reshape(label, (1, num_classes))
+    # Calculate the gradients of the loss with respect to the image.
+    gradients = torch.autograd.grad(loss, processed_image, create_graph=True)[0]
 
-    perturbations = create_adversarial_pattern(image, label, model)
+    # Create the adversarial pattern.
+    adversarial_pattern = epsilon * gradients.sign()
 
-    epsilons = [0, 0.01, 0.1, 0.15]
+    return adversarial_pattern
 
-    adversarial_images = []
-    for eps in epsilons:
-        adv_x = image + eps * perturbations
-        adv_x = tf.clip_by_value(adv_x, -1, 1)
-        adversarial_images.append(adv_x)
-
-    return adversarial_images
-
-# Function to evaluate the model's accuracy and robustness
 def evaluate_model(data_dir):
     st.title("Model Evaluation")
 
     # Load the pretrained ViT model for image classification
     feature_extractor = ViTFeatureExtractor.from_pretrained("rafalosa/diabetic-retinopathy-224-procnorm-vit")
-    model = ViTForImageClassification.from_pretrained("rafalosa/diabetic-retinopathy-224-procnorm-vit")
+    model = ViTImageProcessor.from_pretrained("rafalosa/diabetic-retinopathy-224-procnorm-vit")
 
     # Get the list of folders in the evaluation directory
     eval_folders = [folder for folder in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, folder))]
@@ -58,36 +48,39 @@ def evaluate_model(data_dir):
     selected_class = st.selectbox("Select a class:", eval_folders)
 
     # Get the list of image files in the selected folder
-    image_files = [file for file in os.listdir(os.path.join(data_dir, selected_class)) if file.endswith(".jpeg")]
+    image_files = [file for file in os.listdir(os.path.join(data_dir, selected_class)) if file.endswith(".png")]
 
     # Create a dropdown to select the image
-    selected_image = st.selectbox("Select an image:", image_files)
+    selected_image = st.selectbox("Select an image:", image_files, index=0)  # Set index to 0 to display the first image
 
-    # Load the image
-    image_path = os.path.join(data_dir, selected_class, selected_image)
-    image = Image.open(image_path)
+    # If no image is selected, set the image path to None
+    image_path = os.path.join(data_dir, selected_class, selected_image) if selected_image else None
 
-    # Preprocess the image and convert it to PyTorch tensor
-    processed_image = feature_extractor(images=image, return_tensors="pt")
+    # If an image is selected, display it
+    if image_path and os.path.exists(image_path):
+        # Convert the string to a file object
+        image_file = io.BytesIO(open(image_path, "rb").read())
 
-    # Get the input label of the image (use the folder name directly as the label)
-    label = selected_class
+        # Load the image
+        image = Image.open(image_file)
 
-    # Generate adversarial image
-    perturbations = create_adversarial_pattern(processed_image, label, model)
-    adversarial_image = processed_image + 0.1 * perturbations
-    adversarial_image = torch.clip(adversarial_image, 0.0, 1.0)
+        if image is not None:
+            processed_image = feature_extractor(images=image, return_tensors="pt")
 
-    # Generate adversarial attack using FGSM
-    for adversarial_image_np in generate_adversarial_attack(processed_image, label, model):
-        # Convert the adversarial attack to an image and display it
-        adv_image = Image.fromarray(np.uint8(np.squeeze(adversarial_image_np) * 255.0))
-        st.subheader(f"{selected_class} - Adversarial Image (FGSM)")
-        st.image(adv_image, caption=f"{selected_class} - Adversarial (FGSM)", use_column_width=True)
+            # Generate adversarial attack using FGSM
+            adversarial_image = create_adversarial_pattern(processed_image, selected_class, model, epsilon=0.1)
+
+            # Convert the adversarial attack to an image and display it
+            adv_image = Image.fromarray(np.uint8(np.squeeze(adversarial_image) * 255.0))
+            st.subheader(f"{selected_class} - Adversarial Image (FGSM)")
+            st.image(adv_image, caption=f"{selected_class} - Adversarial (FGSM)", use_column_width=True)
+
+        # Display the original image
+        st.image(image, caption=f"{selected_class} - Original Image")
 
 # Main App
 def main():
-    data_dir = "./images/evaluation2"
+    data_dir = "/Users/kkhan/Desktop/AAI1001/Project Folder/diabetes-data-engineering-project/main/images/evaluation2"
     evaluate_model(data_dir)
 
 if __name__ == "__main__":
